7b7b658993bef6bcf12c316f0cc9398456b8d27b
waldemarciupa <waldemarciupa90@gmail.com>
2022-02-23 15:25:04 +0100

    fetch races

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,43 @@
 import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
 import Layout from '../components/Layout';
 
-const Home: NextPage = () => {
+interface RacesProps {
+  races: {
+    raceName: string;
+    round: string;
+    date: Date;
+    time: Date;
+  }[];
+}
+
+const Home: NextPage<RacesProps> = ({ races }) => {
   return (
     <Layout>
       <h1>Homepage</h1>
+      <ul>
+        {races.map((race) => (
+          <li key={race.round}>
+            <p>{race.raceName}</p>
+            <p>{race.date}</p>
+            <p>{race.time}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('http://ergast.com/api/f1/2022/races.json');
+  const data = await res.json();
+  const races = data.MRData.RaceTable.Races;
+
+  return {
+    props: {
+      races,
+    },
+  };
 };
 
 export default Home;
