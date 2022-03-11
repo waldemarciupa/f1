86adefee73a45d5adcf9ca2a86a8120d11f81f19
waldemarciupa <waldemarciupa90@gmail.com>
2022-03-11 10:44:09 +0100

    fetching single race with graphql

--- pages/races/[id].tsx
+++ pages/races/[id].tsx
@@ -1,15 +1,77 @@
-import { useRouter } from 'next/router';
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import { gql } from '@apollo/client';
+import client from '../../apollo-client';
 import Layout from '../../components/Layout';
 
-const Race = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
+const Race: NextPage = ({ race }) => {
   return (
-    <Layout>
-      <h1>Race id: {id}</h1>
+    <Layout title={race.name}>
+      <h1>Race id: {race.id}</h1>
     </Layout>
   );
+};
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      query Races {
+        races {
+          id
+        }
+      }
+    `,
+  });
+
+  const paths = data.races.map((race: any) => ({
+    params: { id: race.id },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+  const { data } = await client.query({
+    variables: { id: params.id },
+    query: gql`
+      query Race($id: String!) {
+        race(id: $id) {
+          id
+          season
+          round
+          name
+          url
+          circuit {
+            id
+            name
+            location
+            country
+            firstGrandPrix
+            numberOfLaps
+            circuitLength
+            raceDistance
+            url
+          }
+          sessions {
+            fp1
+            fp2
+            fp3
+            qualifying
+            gp
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      race: data.race,
+    },
+  };
 };
 
 export default Race;
